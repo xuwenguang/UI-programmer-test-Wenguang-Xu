5fb2b3c53ebd7f1bb2720004c4dc4837b099e451
Wenguang Xu <Wenguang_Xu@gnwc.ca>
2014-07-13 22:59:34 -0700

    finished calculator and questions
    done

---
+++ 3DCalculator/Assets/scripts/Calculator.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;  
+using System;
+using System.Collections;  
+
+
+public class Calculator : MonoBehaviour { 
+	
+	
+	public string strPutkeyCode;  
+	
+	private UILabel strResult;   //result label  
+	
+	public static string str1;  //the first input number  
+	
+	public static string str2;  //second number  
+	
+	public static string strOpt;    //operators  
+	
+	
+	int sum = 0; 
+
+	void Start () { 
+		strResult=GameObject.Find("2dDisplay/Label").GetComponent<UILabel>();
+	} 
+
+	
+	
+	void OnMouseDown() 
+		
+	{ 
+		Debug.Log ("clicked");
+		try
+		{
+			if(strPutkeyCode == "=") 
+				
+			{ 
+				if(strOpt == "/") 		
+				{ 
+					sum =int.Parse(str2)/int.Parse(str1);  
+				} 
+				
+				else if(strOpt == "*") 
+				{ 
+					sum =int.Parse(str1)*int.Parse(str2);  	
+				} 
+				else if(strOpt == "+") 	
+				{ 
+					sum =int.Parse(str1)+int.Parse(str2);  	
+				} 
+				else if(strOpt == "-") 	
+				{ 
+					sum =int.Parse(str2)-int.Parse(str1);  	
+				} 
+				
+				str1 = "";  
+				
+				str2 = sum.ToString();  
+				
+				Debug.Log(sum);  
+				
+				strResult.text = sum.ToString();  
+				
+			}
+		}
+		catch(Exception e)
+		{}
+
+		if(strPutkeyCode == "-"||strPutkeyCode == "+") 
+			
+		{ 
+			
+			strOpt=strPutkeyCode;   
+			if(str1 != "") 
+				
+			{ 
+				
+				str2=str1;    
+				
+			} 
+			
+			
+			strResult.text = str2;  
+			
+			Debug.Log("code---");  
+			
+			str1="";  
+			
+		} 
+
+		if(strPutkeyCode =="1" ||strPutkeyCode =="2" ||strPutkeyCode =="3" ||strPutkeyCode =="4" ||  
+		   
+		   strPutkeyCode =="5" ||strPutkeyCode =="6" ||strPutkeyCode =="7" ||strPutkeyCode =="8" ||  
+		   
+		   strPutkeyCode =="9" ||strPutkeyCode =="0") 
+			
+		{ 
+			
+			str1 += strPutkeyCode;  
+			
+			strResult.text = str1;  
+			
+			Debug.Log(str1);  
+			
+		} 
+		
+	} 
+
+	//for the clear button
+	public void clear()
+	{
+		Debug.Log("CE");  
+		
+		strResult.text = "0";  
+		
+		sum = 0;  
+		
+		str1 = "";  
+		
+		str2 = "";  
+	}
+} 
---
+++ 3DCalculator/Assets/scripts/UIManager.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject[] calculatorLayouts;
+	public GameObject[] wizards;
+	private int currentWizardIndex=0;
+	private bool inCalculator=false;
+	private string calculatorLayout="line_layout";
+	private string name;
+	public GameObject nameLabel;
+	public GameObject layoutLabel;
+	public GameObject calculatorDisplay;
+
+	// Use this for initialization
+	void Start () {
+		//set up the default calculator layout
+		layoutLabel.GetComponent<UILabel> ().text = calculatorLayout;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void next()
+	{
+		if(!inCalculator)
+		{
+			wizards [currentWizardIndex].SetActive (false);
+			if(currentWizardIndex<wizards.Length-1)
+			{
+				currentWizardIndex++;
+			}
+			wizards[currentWizardIndex].SetActive(true);
+		}
+	}
+
+	public void back()
+	{
+		if(inCalculator)
+		{
+			hideCalculator();
+			inCalculator=false;
+			hideCalculator();
+		}
+
+		wizards [currentWizardIndex].SetActive (false);
+		if(currentWizardIndex>0)
+		{
+			currentWizardIndex--;
+		}
+		wizards [currentWizardIndex].SetActive (true);
+	}
+
+	public void startCalculator()
+	{
+		inCalculator = true;
+		wizards [currentWizardIndex].SetActive (false);
+		showCalculator ();
+	}
+
+	public void clear()
+	{
+
+	}
+
+	public void showCalculator()
+	{
+		calculatorDisplay.SetActive (true);
+		switch (calculatorLayout)
+		{
+		case "tranditional_layout" :
+			for(int i=0; i<calculatorLayouts.Length;i++)
+			{
+				if(calculatorLayouts[i].name.Contains("tranditional"))
+				{
+					calculatorLayouts[i].SetActive(true);
+				}
+			}
+			break;
+		case "line_layout" :
+			for(int i=0; i<calculatorLayouts.Length;i++)
+			{
+				if(calculatorLayouts[i].name.Contains("line"))
+				{
+					calculatorLayouts[i].SetActive(true);
+				}
+			}
+			break;
+
+		}
+
+	}
+
+	public void hideCalculator()
+	{
+		for(int i=0; i<calculatorLayouts.Length;i++)
+		{
+			calculatorLayouts[i].SetActive(false);
+		}
+		calculatorDisplay.SetActive (false);
+	}
+
+	public void setTranditionalLayout()
+	{
+		if (calculatorLayout != "tranditional_layout")
+			calculatorLayout = "tranditional_layout";
+		layoutLabel.GetComponent<UILabel> ().text = calculatorLayout;
+	}
+
+	public void setLineLayout()
+	{
+		if (calculatorLayout != "line_layout")
+			calculatorLayout = "line_layout";
+		layoutLabel.GetComponent<UILabel> ().text = calculatorLayout;
+	}
+
+	public void inputName()
+	{
+		//get text from nameInputField
+		name =GameObject.Find ("nameInputField/Label").GetComponent<UILabel>().text;
+		nameLabel.GetComponent<UILabel> ().text = name;
+	}
+}
